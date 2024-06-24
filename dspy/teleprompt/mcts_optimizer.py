d895e2d78e283d50ffcd7278f0b7faa298fb7b9b
Callum Thickett <cthickett99@hotmail.com>
2024-06-24 15:01:58 +0100

    feat: Add tree structure and MCTS initialization in preperation for compile method. added corrasponding tests.
    - Introduced `Node` class to define the structure for storing and managing instructions, scores, and feedback.
      - Attributes include `module`, `instructions`, `score`, `feedback`, `allowed_retries`, `is_root`, and relationships (`parent` and `children`).
      - Added methods for adding parent and child nodes.
    - Added `MCTSTree` class to represent the optimizer's structure.
      - Initialized with root node using basic_instructions and allows adding nodes recursively.
    - Implemented `create_tree` method in the `MCTS` class to initialize and build the tree with specified breadth and depth.
    - Added tests for the creation of the tree structure
     - check for correct number of relationships between nodes
     - check for correct module initialised
     - check that parameters are correctly initialized
    
    References:
    - DeepMind's paper, PROMPTBREEDER (https://arxiv.org/abs/2309.16797)

--- dspy/teleprompt/mcts_optimizer.py
+++ dspy/teleprompt/mcts_optimizer.py
@@ -10,33 +10,107 @@ from dspy.teleprompt.teleprompt import Teleprompter
 # mutated prompts taken from DeepMind's paper, PROMPTBREEDER
 # https://arxiv.org/abs/2309.16797
 mutated_prompts = [
-    'Modify the following instruction creatively, giving some advice on how to solve\nit:',
-    'Just change this instruction to make it more fun, think WELL outside the box:',
-    'Don’t think about the instruction at all, but let it inspire you to do something\nrelated. Talk about what that might be'
+    "Modify the following instruction creatively, giving some advice on how to solve\nit:",
+    "Just change this instruction to make it more fun, think WELL outside the box:",
+    "Don’t think about the instruction at all, but let it inspire you to do something\nrelated. Talk about what that might be",
 ]
 
 
 class BasicGenerateInstruction(Signature):
-
     """You are an instruction optimizer for large language models. I will give you a ``signature`` of fields (inputs and outputs) in English. Your task is to propose an instruction that will lead a good language model to perform the task well. Don't be afraid to be creative."""
 
-    basic_instruction = dspy.InputField(desc="The initial instructions before optimization")
-    proposed_instruction = dspy.OutputField(desc="The improved instructions for the language model")
+    basic_instruction = dspy.InputField(
+        desc="The initial instructions before optimization"
+    )
+    proposed_instruction = dspy.OutputField(
+        desc="The improved instructions for the language model"
+    )
     proposed_prefix_for_output_field = dspy.OutputField(
         desc="The string at the end of the prompt, which will help the model start solving the task",
     )
+
 
 class GenerateInstructionGivenAttempts(dspy.Signature):
     """You are an instruction optimizer for large language models. I will give some task instructions I've tried, along with their corresponding validation scores. The instructions are arranged in increasing order based on their scores, where higher scores indicate better quality.
 
-    Your task is to propose a new instruction that will lead a good language model to perform the task even better. Don't be afraid to be creative."""
+    Your task is to propose a new instruction that will lead a good language model to perform the task even better. Don't be afraid to be creative.
+    """
 
     attempted_instruction = dspy.InputField(desc="The previous instruction used.")
-    Instructions_feedback = dspy.InputField(desc="The validation score and reasoning given for the attempted instructions")
-    proposed_instruction = dspy.OutputField(desc="The improved instructions for the language model")
+    Instructions_feedback = dspy.InputField(
+        desc="The validation score and reasoning given for the attempted instructions"
+    )
+    proposed_instruction = dspy.OutputField(
+        desc="The improved instructions for the language model"
+    )
     proposed_prefix_for_output_field = dspy.OutputField(
         desc="The string at the end of the prompt, which will help the model start solving the task",
     )
+
+
+def generate_mutated_signiture(signiture, mutated_signiture):
+    signiture.__doc__ = mutated_signiture
+
+
+# define data structure used for storing the abstracted program.
+class Node:
+    def __init__(
+        self,
+        module,
+        instructions,
+        score=None,
+        feedback=None,
+        allowed_retries=3,
+        is_root=False,
+    ):
+        self.is_root = is_root
+        self.compiled = False
+        self.module = module
+        self.instructions = instructions
+        self.mutated_prompts = random.sample(mutated_prompts, allowed_retries)
+        self.prompt_crossover = random.random() > 0.9  # 10% chance of crossover
+        self.score = score
+        self.feedback = feedback
+        self.retries = allowed_retries
+        self.parent = None
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+
+    def add_parent(self, parent_node):
+        self.parent = parent_node
+
+    def __repr__(self):
+        return f"Node(instructions={self.instructions}, score={self.score}, feedback={self.feedback}, retries={self.retries})"
+
+
+# logic is that we will define the tree before computing, i,e we have an abstracted tree structure
+# this allows the user to make changes, add to the tree, or do what ever else they may want to do
+# we then compile this abstract tree in parallel, i.e each branch is compiled in parallel.
+class MCTSTree:
+    def __init__(self, module, basic_instruction):
+        self.module = module
+        self.root = Node(
+            is_root=True,
+            module=self.module.deepcopy(),
+            instructions=basic_instruction,
+        )
+
+    def add_node(self, parent_node):
+        new_node = Node(
+            module=self.module.deepcopy(),
+            instructions=None,  # instructions are defined during compilation
+        )
+        parent_node.add_child(new_node)
+        new_node.add_parent(parent_node)
+        return new_node
+
+    def add_nodes_recursively(self, node, current_depth, max_depth):
+        if current_depth < max_depth:
+            new_node = self.add_node(node)
+            self.add_nodes_recursively(new_node, current_depth + 1, max_depth)
+
 
 class MCTS(Teleprompter):
     def __init__(
@@ -48,10 +122,10 @@ class MCTS(Teleprompter):
         depth=3,
         init_temperature=1.4,
         track_stats=False,
-        **_kwargs
+        **_kwargs,
     ):
         # if breadth==1 we simply aim to optimise the initital prompt.
-        if breadth <1:
+        if breadth < 1:
             raise ValueError("breadth must be greater than 0")
         self.prompt_model = prompt_model
         self.prune_rate = prune_rate
@@ -60,6 +134,10 @@ class MCTS(Teleprompter):
         self.depth = depth
         self.init_temperature = init_temperature
         self.track_stats = track_stats
+        self.tree = None
 
-
-
+    def create_tree(self, student, basic_instruction):
+        self.tree = MCTSTree(student, basic_instruction)
+        for _ in range(self.breadth - 1):
+            child = self.tree.add_node(self.tree.root)
+            self.tree.add_nodes_recursively(child, 1, self.depth)
--- tests/teleprompt/test_mcts_optimizer.py
+++ tests/teleprompt/test_mcts_optimizer.py
@@ -4,19 +4,72 @@ from dspy.teleprompt.mcts_optimizer import MCTS
 from dspy.utils.dummies import DummyLM
 from dspy import Example
 
+
 # Define a simple metric function for testing
 def simple_metric(example, prediction):
     # Simplified metric for testing: true if prediction matches expected output
     return example.output == prediction.output
 
+
 trainset = [
-    Example(input="Question: What is the color of the sky?", output="blue").with_inputs("input"),
-    Example(input="Question: What does the fox say?", output="Ring-ding-ding-ding-dingeringeding!").with_inputs("input"),
+    Example(input="Question: What is the color of the sky?", output="blue").with_inputs(
+        "input"
+    ),
+    Example(
+        input="Question: What does the fox say?",
+        output="Ring-ding-ding-ding-dingeringeding!",
+    ).with_inputs("input"),
 ]
+
+
+class SimpleModule(dspy.Module):
+    def __init__(self, signature):
+        super().__init__()
+        # MCTS doesn't work with dspy.Predict
+        self.predictor = dspy.ChainOfThought(signature)
+
+    def forward(self, **kwargs):
+        return self.predictor(**kwargs)
+
 
 def test_signature_optimizer_initialization():
     optimizer = MCTS(metric=simple_metric, breadth=2, depth=1, init_temperature=1.4)
     assert optimizer.metric == simple_metric, "Metric not correctly initialized"
     assert optimizer.breadth == 2, "Breadth not correctly initialized"
     assert optimizer.depth == 1, "Depth not correctly initialized"
-    assert optimizer.init_temperature == 1.4, "Initial temperature not correctly initialized"
+    assert (
+        optimizer.init_temperature == 1.4
+    ), "Initial temperature not correctly initialized"
+
+
+def test_mctc_tree_initialization():
+    optimizer = MCTS(metric=simple_metric, breadth=2, depth=1, init_temperature=1.4)
+    module = SimpleModule("input -> output")
+    optimizer.create_tree(module, "Question: What is the color of the sky?")
+    assert (
+        optimizer.tree.root.instructions == "Question: What is the color of the sky?"
+    ), "Root node instructions not correctly initialized"
+    assert (
+        len(optimizer.tree.root.children) == 1
+    ), "Root node children not correctly initialized. should be breadth - 1."
+    # add node to root
+    optimizer.tree.add_node(optimizer.tree.root)
+    assert (
+        len(optimizer.tree.root.children) == 2
+    ), f"Node not correctly added to root, got {len(optimizer.tree.root.children)} children but should have breadth -1 + 1"
+    assert (
+        optimizer.tree.root.children[0].parent == optimizer.tree.root
+    ), "Parent not correctly set"
+    assert isinstance(
+        optimizer.tree.root.module, SimpleModule
+    ), "Root node's module is not of type SimpleModule"
+    for children in optimizer.tree.root.children:
+        assert isinstance(
+            children.module, SimpleModule
+        ), "child node's module is not of type SimpleModule"
+
+
+if __name__ == "__main__":
+    test_signature_optimizer_initialization()
+    test_mctc_tree_initialization()
+    print("All tests passed!")
